7ea3170170dc63cb9de0eb0b81a42f167f13050b
Wilker Bruno <wilkerbruno16@gmail.com>
2021-04-02 15:47:29 -0300

    feat: add GoogleMaps to OngDetalhes fragment

--- app/src/main/java/com/example/doacao_ong/ui/usuario/ong_detalhes/OngDetalhesFragment.java
+++ app/src/main/java/com/example/doacao_ong/ui/usuario/ong_detalhes/OngDetalhesFragment.java
@@ -1,7 +1,10 @@
 package com.example.doacao_ong.ui.usuario.ong_detalhes;
 
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,15 +18,20 @@ import android.view.ViewGroup;
 import com.example.doacao_ong.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class OngDetalhesFragment extends Fragment {
 
     private OngDetalhesViewModel mViewModel;
-    private GoogleMap mMap;
+    private MapView mapView;
+    private GoogleMap googleMap;
 
     public static OngDetalhesFragment newInstance() {
         return new OngDetalhesFragment();
@@ -33,6 +41,47 @@ public class OngDetalhesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.ong_detalhes_fragment, container, false);
+
+        mapView = rootView.findViewById(R.id.ong_detalhes_map_view);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+                System.out.println("-----------> getMapAsync");
+
+                if (ActivityCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                    }, PackageManager.PERMISSION_GRANTED);
+                }
+
+                googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+//                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
 
 //        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
 //                .findFragmentById(R.id.layout_map);
@@ -46,6 +95,30 @@ public class OngDetalhesFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(OngDetalhesViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 
 //    @Override
